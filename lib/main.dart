7301e651aea7bc813f6ecb3f00ce2d9b57581bc8
Sajal Kumar Jana <thehackertimes2k23@gmail.com>
2024-10-07 13:44:08 +0530

    added env files

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hushhxtinder/firebase_options.dart';
 import 'package:hushhxtinder/ui/app/chat/chatViewModel.dart';
@@ -37,6 +38,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:uni_links2/uni_links.dart';
 
 void main() async {
+  await dotenv.load(fileName: "lib/assets/.env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
